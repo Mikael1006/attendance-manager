48176348e9a00fe0bf00ac345c1d165247839703
Mikael1006 <mikael.moreau1006@gmail.com>
2017-10-11 19:23:27 +0200

    Select date match activity finished

--- app/src/main/java/com/gmail/moreau1006/mikael/attendancemanager/Activity/SelectDateMatchActivity.java
+++ app/src/main/java/com/gmail/moreau1006/mikael/attendancemanager/Activity/SelectDateMatchActivity.java
@@ -1,80 +1,123 @@
 package com.gmail.moreau1006.mikael.attendancemanager.Activity;
 
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
+import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
+import com.codetroopers.betterpickers.radialtimepicker.RadialTimePickerDialogFragment;
+import com.gmail.moreau1006.mikael.attendancemanager.Model.Match;
 import com.gmail.moreau1006.mikael.attendancemanager.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class SelectDateMatchActivity extends AppCompatActivity {
 
-    TextView date;
-    DatePickerDialog datePickerDialog;
-
-    TextView time;
-    TimePickerDialog timePickerDialog;
+    private static final String FRAG_TAG_DATE_PICKER = "datePickerMatch";
+    private static final String FRAG_TAG_TIME_PICKER = "timePickerMatch";
+    private TextView dateTextView;
+    private TextView timeTextView;
+    private Match match;
+    private String date;
+    private String time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        // http://abhiandroid.com/ui/datepicker
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_date_match);
 
-        // initiate the date picker and a button
-        date = (TextView) findViewById(R.id.date_match_editText);
-        time = (TextView) findViewById(R.id.time_match_editText);
+        // initiate TextViews
+        dateTextView = (TextView) findViewById(R.id.date_match_editText);
+        timeTextView = (TextView) findViewById(R.id.time_match_editText);
 
+        date = null;
+        time = null;
+
+        match = new Match();
     }
 
     public void onClickDate(View view){
-        // calender class's instance and get current date , month and year from calender
+
+        // https://stackoverflow.com/questions/27362971/android-material-design-datepicker-with-appcompat
+
+        // calender class's instance and get current dateTextView , month and year from calender
         final Calendar c = Calendar.getInstance();
         int mYear = c.get(Calendar.YEAR); // current year
         int mMonth = c.get(Calendar.MONTH); // current month
         int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-        // date picker dialog
-        datePickerDialog = new DatePickerDialog(SelectDateMatchActivity.this, AlertDialog.THEME_DEVICE_DEFAULT_DARK,
-                new DatePickerDialog.OnDateSetListener() {
+
+        //MonthAdapter.CalendarDay minDate = new MonthAdapter.CalendarDay();
+
+        CalendarDatePickerDialogFragment cdp = new CalendarDatePickerDialogFragment()
+                .setOnDateSetListener(new CalendarDatePickerDialogFragment.OnDateSetListener() {
 
                     @Override
-                    public void onDateSet(DatePicker view, int year,
+                    public void onDateSet(CalendarDatePickerDialogFragment view, int year,
                                           int monthOfYear, int dayOfMonth) {
+                        date = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+
                         // set day of month , month and year value in the edit text
-                        date.setText(dayOfMonth + "/"
-                                + (monthOfYear + 1) + "/" + year);
+                        dateTextView.setText(date);
 
                     }
-                }, mYear, mMonth, mDay);
+                })
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setPreselectedDate(mYear, mMonth, mDay)
+                .setDateRange(null, null)
+                .setDoneText("Valider")
+                .setCancelText("Annuler");
+        cdp.show(getSupportFragmentManager(), FRAG_TAG_DATE_PICKER);
 
-        datePickerDialog.show();
     }
 
     public void onClickTime(View view){
-        final Calendar mcurrentTime = Calendar.getInstance();
-        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-        int minute = mcurrentTime.get(Calendar.MINUTE);
 
-        timePickerDialog = new TimePickerDialog(SelectDateMatchActivity.this,
-                new TimePickerDialog.OnTimeSetListener() {
+        RadialTimePickerDialogFragment rtpd = new RadialTimePickerDialogFragment()
+                .setOnTimeSetListener(new RadialTimePickerDialogFragment.OnTimeSetListener() {
 
                     @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        time.setText( selectedHour + ":" + selectedMinute);
+                    public void onTimeSet(RadialTimePickerDialogFragment view, int selectedHour, int selectedMinute) {
+                        time = selectedHour + ":" + selectedMinute;
+                        // set time in text view
+                        timeTextView.setText(time);
                     }
 
-                }, hour, minute, true);
+                })
+                .setStartTime(10, 10)
+                .setDoneText("Valider")
+                .setCancelText("Annuler");
+        rtpd.show(getSupportFragmentManager(), FRAG_TAG_TIME_PICKER);
+    }
 
-        timePickerDialog.show();
+    public void validateDateMatch(View view){
+
+        if (date != null && time != null){
+            Date dateMatch = null;
+            try {
+                dateMatch = new SimpleDateFormat("dd/MM/yyyy, HH:mm").parse(date + ", " + time);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            match.setDateMatch(dateMatch);
+
+
+        }else{
+            AlertDialog alertDialog = new AlertDialog.Builder(SelectDateMatchActivity.this).create();
+            alertDialog.setTitle("Attention");
+            alertDialog.setMessage("Veuillez selectionner une date et une heure de match");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
     }
 }
